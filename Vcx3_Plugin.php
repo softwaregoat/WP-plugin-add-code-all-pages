668e99543a8ede695827ebff77da1860d67f8562
Software.Goat <61810640+softwaregoat@users.noreply.github.com>
2022-02-10 04:32:21 +0800

    First add
    First add full plugin.

---
+++ Vcx3_Plugin.php
@@ -0,0 +1,128 @@
+<?php
+
+
+include_once('Vcx3_LifeCycle.php');
+
+class Vcx3_Plugin extends Vcx3_LifeCycle {
+
+    /**
+     * See: http://plugin.michael-simpson.com/?page_id=31
+     * @return array of option meta data.
+     */
+    public function getOptionMetaData() {
+        //  http://plugin.michael-simpson.com/?page_id=31
+        return array(
+            //'_version' => array('Installed Version'), // Leave this one commented-out. Uncomment to test upgrades.
+            'my_js_snippet_email' => array(__('Enter your email address', 'my-awesome-plugin')),
+        );
+    }
+
+//    protected function getOptionValueI18nString($optionValue) {
+//        $i18nValue = parent::getOptionValueI18nString($optionValue);
+//        return $i18nValue;
+//    }
+
+    protected function initOptions() {
+        $options = $this->getOptionMetaData();
+        if (!empty($options)) {
+            foreach ($options as $key => $arr) {
+                if (is_array($arr) && count($arr > 1)) {
+                    $this->addOption($key, $arr[1]);
+                }
+            }
+        }
+    }
+
+    public function getPluginDisplayName() {
+        return 'vcx3';
+    }
+
+    protected function getMainPluginFileName() {
+        return 'vcx3.php';
+    }
+
+    /**
+     * See: http://plugin.michael-simpson.com/?page_id=101
+     * Called by install() to create any database tables if needed.
+     * Best Practice:
+     * (1) Prefix all table names with $wpdb->prefix
+     * (2) make table names lower case only
+     * @return void
+     */
+    protected function installDatabaseTables() {
+        //        global $wpdb;
+        //        $tableName = $this->prefixTableName('mytable');
+        //        $wpdb->query("CREATE TABLE IF NOT EXISTS `$tableName` (
+        //            `id` INTEGER NOT NULL");
+    }
+
+    /**
+     * See: http://plugin.michael-simpson.com/?page_id=101
+     * Drop plugin-created tables on uninstall.
+     * @return void
+     */
+    protected function unInstallDatabaseTables() {
+        //        global $wpdb;
+        //        $tableName = $this->prefixTableName('mytable');
+        //        $wpdb->query("DROP TABLE IF EXISTS `$tableName`");
+    }
+
+
+    /**
+     * Perform actions when upgrading from version X to version Y
+     * See: http://plugin.michael-simpson.com/?page_id=35
+     * @return void
+     */
+    public function upgrade() {
+    }
+
+    public function addActionsAndFilters() {
+
+        // Add options administration page
+        // http://plugin.michael-simpson.com/?page_id=47
+        add_action('admin_menu', array(&$this, 'addSettingsSubMenuPage'));
+
+        // Example adding a script & style just for the options administration page
+        // http://plugin.michael-simpson.com/?page_id=47
+        //        if (strpos($_SERVER['REQUEST_URI'], $this->getSettingsSlug()) !== false) {
+        //            wp_enqueue_script('my-script', plugins_url('/js/my-script.js', __FILE__));
+        //            wp_enqueue_style('my-style', plugins_url('/css/my-style.css', __FILE__));
+        //        }
+
+
+        // Add Actions & Filters
+        // http://plugin.michael-simpson.com/?page_id=37
+        $optionMetaData = $this->getOptionMetaData();
+
+        // Save Posted Options
+        if ($optionMetaData != null) {
+            foreach ($optionMetaData as $aOptionKey => $aOptionMeta) {
+                if ($this->getOption($aOptionKey)!=='') {
+                    add_action('wp_head', array(&$this, 'add_my_js_snippet'));
+                }
+            }
+        }
+
+        // Adding scripts & styles to all pages
+        // Examples:
+        //        wp_enqueue_script('jquery');
+        //        wp_enqueue_style('my-style', plugins_url('/css/my-style.css', __FILE__));
+        //        wp_enqueue_script('my-script', plugins_url('/js/my-script.js', __FILE__));
+
+
+        // Register short codes
+        // http://plugin.michael-simpson.com/?page_id=39
+
+
+        // Register AJAX hooks
+        // http://plugin.michael-simpson.com/?page_id=41
+
+    }
+    function add_my_js_snippet(){
+        ?>
+        <script type="text/javascript" src="https://www.visualclicks.de/tracker/tracker.js"></script>
+<?php
+    }
+
+
+}
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== vcx3 ===
+Contributors: Software.Goat
+Donate link:
+Tags:
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Requires at least: 3.5
+Tested up to: 3.5
+Stable tag: 0.1
+
+To run Js code for all page
+
+== Description ==
+
+To run Js code for all page
+
+== Installation ==
+
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 0.1 =
+- Initial Revision
